a6bfe01c7a90a127b3c814cd5096b0157d6e7bf8
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-04 17:04:36 +0300

    feat: add read metod

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+const pathFile = path.resolve('files', 'fileToRead.txt');
+
+const readStream = fs.createReadStream(pathFile, 'utf-8');
+
+readStream
+.on('data', (chunk) => console.log(chunk))
+.on('error', () => console.error('\x1b[31m',new Error('FS operation failed')));
 };
+
+read();
