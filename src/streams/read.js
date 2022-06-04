fbc7adcf0909e1e08ee8c090f610a1d7b9447be9
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 02:58:42 +0300

    feat: add readStream metod

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
 export const read = async () => {
-    // Write your code here 
+    const pathFile = path.resolve('files', 'fileToRead.txt');
+
+const readStream = fs.createReadStream(pathFile, 'utf-8');
+
+readStream
+.on('data', (chunk) => console.log(chunk))
+.on('error', (error) => console.error('\x1b[31m','Error', error.message));
 };
+
+read();
