3d69cab0eff39545c88a43ff12f3ad4708a09b55
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-04 19:23:35 +0300

    feat: add list metod

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,14 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
 export const list = async () => {
-    // Write your code here 
+    const outFolder = path.resolve('files');
+    try {
+        const files = await readdir(outFolder);
+    console.log(files.toString());
+    } catch (error) {
+        console.error('\x1b[31m',new Error('FS operation failed'))
+    }
 };
+
+list();
