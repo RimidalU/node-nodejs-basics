386062e587c9fa8bbca24f1b8e7dcf8523a13222
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-04 21:48:41 +0300

    feat: add rename metod

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,17 @@
+import { rename as renameFile } from 'fs/promises';
+import { access, constants } from 'fs';
+import path from 'path';
+
 export const rename = async () => {
-    // Write your code here 
+    const oldFile = path.resolve('files', 'wrongFilename.txt');
+    const newFile = path.resolve('files', 'properFilename.md');
+    
+    try {
+        access(newFile, constants.F_OK, (err) => err)
+        await renameFile(oldFile, newFile)    
+    } catch (error) {
+        console.error('\x1b[31m',new Error('FS operation failed'))
+    }
 };
+
+rename();
