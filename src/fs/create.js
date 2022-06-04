0d4127a44357f521ad67cac5cfd5d6e019d7a5ea
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-04 18:46:09 +0300

    feat: add create  metod

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,13 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
 export const create = async () => {
-    // Write your code here 
+    const outFile = path.resolve('files', 'fresh.txt');
+    try {
+        await writeFile(outFile, 'I am fresh and young', { flag: 'wx' });
+    } catch (err) {
+        console.error('\x1b[31m',new Error('FS operation failed'))
+    }
 };
+
+create()
