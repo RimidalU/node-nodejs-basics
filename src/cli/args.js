415dff0fda0fc47a7a61a59a10ba6edb2bc8f43e
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 00:43:47 +0300

    feat: add args metod

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,11 @@
+import { argv } from 'node:process'
+
 export const parseArgs = () => {
-    // Write your code here 
+    const argvArr = argv.slice(2);
+
+    for (let i = 0; i < argv.length - 2; i = i + 2)
+        if (argvArr[i].slice(0, 2) == '--') { console.log(`${argvArr[i]} is ${argvArr[i + 1]}`) }
+        else { i = i - 1 }
 };
+
+parseArgs();
