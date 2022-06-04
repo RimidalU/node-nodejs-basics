9c1bdc90c446ade8d8f4b835610aa9c29ce4dbde
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-04 23:26:32 +0300

    feat: add copy metod

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,18 @@
+import { cp } from 'fs/promises';
+import path from 'path';
+
 export const copy = async () => {
-    // Write your code here 
+    const outFolder = path.resolve('files');
+    const inFolder = path.resolve('files_copy');
+    try {
+        await cp(outFolder, inFolder, {
+            errorOnExist: true,
+            force: false,
+            recursive: true
+        })
+    } catch (error) {
+        console.error('\x1b[31m', new Error('FS operation failed'))
+    }
 };
+
+copy();
