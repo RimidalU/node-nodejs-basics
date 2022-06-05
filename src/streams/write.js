9c2d374165d38147092af04e0cbd2256c83fe958
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 03:11:55 +0300

    feat: add writeStream metod

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import readLine from 'readline';
+
 export const write = async () => {
-    // Write your code here 
+    const pathFile = path.resolve('files', 'fileToWrite.txt');
+
+    const consoleStream = readLine.createInterface(process.stdin, process.stdout);
+    const writeStream = fs.createWriteStream(pathFile, 'utf-8');
+
+    console.log('Привет! Начнём тест! Введи, пожалуйста, текст: \n')
+
+    function exit() {
+        console.log('Ввод текста закончен. До свидания! \n');
+        consoleStream.close()
+    };
+
+    consoleStream
+        .on('line', (data) => {
+            if (data === 'exit' || data === 'Exit') {
+                exit()
+            }
+            else {
+                console.log('Продолжим ввод?(для выхода набери exit или нажми ctrl+c): \n')
+                writeStream.write(`${data} \n`)
+            }
+        })
+
+        .on('SIGINT', () => { exit() })
+        .on('error', error => console.log('Error', error.message));
 };
+
+
+write()
