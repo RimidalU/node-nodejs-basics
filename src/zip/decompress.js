1086a3d3199c73cc30a1a1905203c676eeaef0a9
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 12:29:18 +0300

    feat: add decompress metod

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import path from 'path';
+import { createUnzip } from 'zlib'
+
 export const decompress = async () => {
-    // Write your code here 
+    const pipelineAsync = promisify(pipeline);
+    const gzip = createUnzip();
+
+    const readableFile = path.resolve('files', 'archive.gz');
+    const writableFile = path.resolve('files', 'fileToCompress.txt');
+
+    const readable = createReadStream(readableFile);
+    let writable = createWriteStream(writableFile);
+
+    try {
+        await pipelineAsync(
+            readable,
+            gzip,
+            writable
+        );
+        console.log("Decompress accomplished!");
+    } catch (error) {
+        console.error('\x1b[31m', 'Decompress failed with error:', error);
+    } 
 };
+
+decompress();
