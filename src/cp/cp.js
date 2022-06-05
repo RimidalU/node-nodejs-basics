551c88daa3274946163403957ba16fdb0ac4544c
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 20:22:02 +0300

    feat: add cp  metod

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
+import { spawn } from "child_process";
+import path from 'path';
+import { argv } from 'process'
+
+const dataToChild = 'child process stdin receives input from master process stdin' 
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptFile = path.resolve('files', 'script.js');
+    const child = spawn('node', [scriptFile, args]);
+
+    //child process stdout sends data to master process stdout
+    child.stdout.pipe(process.stdout);
+
+    //child process stdin receives input from master process stdin
+    child.stdin.write(dataToChild);
+
+    child.stdin.end();
 };
+
+spawnChildProcess(argv);
