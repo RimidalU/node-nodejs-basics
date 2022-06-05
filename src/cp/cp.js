33a849e24283c836e50eaa070f177df650a6172a
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 21:02:23 +0300

    fix: fix cp  metod

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,19 +2,11 @@ import { spawn } from "child_process";
 import path from 'path';
 import { argv } from 'process'
 
-const dataToChild = 'child process stdin receives input from master process stdin' 
-
 export const spawnChildProcess = async (args) => {
     const scriptFile = path.resolve('files', 'script.js');
     const child = spawn('node', [scriptFile, args]);
-
-    //child process stdout sends data to master process stdout
     child.stdout.pipe(process.stdout);
-
-    //child process stdin receives input from master process stdin
-    child.stdin.write(dataToChild);
-
-    child.stdin.end();
+    process.stdin.pipe(child.stdin);
 };
 
 spawnChildProcess(argv);
