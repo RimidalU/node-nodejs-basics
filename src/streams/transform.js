4eb01dfcb00de6c8943a188c372497c4272cb69e
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 11:39:01 +0300

    feat: add transformStream metod

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,37 @@
+// import { createReadStream, createWriteStream } from 'fs';
+// import path from 'path';
+import { stdin, stdout } from 'process'
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { Transform } from "stream";
+
+
 export const transform = async () => {
-    // Write your code here 
-};
+
+    const pipelineAsync = promisify(pipeline);
+    const readable = stdin;
+    let writable = stdout;
+
+    // const readableFile = path.resolve('files', 'fileToRead.txt');
+    // const writableFile = path.resolve('files', 'output.text');
+    // const readable = createReadStream(readableFile);
+    // let writable = createWriteStream(writableFile);
+
+    const reverseChunk = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        },
+    });
+    try {
+        await pipelineAsync(
+            readable,
+            reverseChunk,
+            writable
+        );
+        console.log("Transform accomplished!");
+    } catch (error) {
+        console.error('\x1b[31m', 'Transform failed with error:', error);
+    }
+}
+
+transform();
