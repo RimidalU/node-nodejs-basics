22a087e949c28a408f3263372ceb8ed2b98b9d3c
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-06-05 12:02:52 +0300

    feat: add compress metod

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,30 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import path from 'path';
+import { createGzip } from 'zlib'
+
+
 export const compress = async () => {
-    // Write your code here 
+    const pipelineAsync = promisify(pipeline);
+    const gzip = createGzip();
+
+    const readableFile = path.resolve('files', 'fileToCompress.txt');
+    const writableFile = path.resolve('files', 'archive.gz');
+
+    const readable = createReadStream(readableFile);
+    let writable = createWriteStream(writableFile);
+
+    try {
+        await pipelineAsync(
+            readable,
+            gzip,
+            writable
+        );
+        console.log("Compress accomplished!");
+    } catch (error) {
+        console.error('\x1b[31m', 'Compress failed with error:', error);
+    }
 };
+
+compress();
